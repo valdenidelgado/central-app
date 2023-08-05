c763ee3c6d83e2aebf8c97c2d341c47360648405
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-05 19:51:47 -0300

    chore: start new project

--- server/domain/src/main/java/com/enterprise/example/AggregateRoot.java
+++
@@ -1,8 +0,0 @@
-package com.enterprise.example;
-
-public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
-
-    public AggregateRoot(ID id) {
-        super(id);
-    }
-}
--- server/domain/src/main/java/com/enterprise/example/ValueObject.java
+++
@@ -1,4 +0,0 @@
-package com.enterprise.example;
-
-public abstract class ValueObject {
-}
